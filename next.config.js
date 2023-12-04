32c9fd2edf120159354524a8c19b4950a5ac434b
Abhishek Faya <abhishek.sk@fayausa.com>
2023-12-04 11:32:38 +0530

    feat(core): update build config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    distDir: 'build',
-    basePath: '/age-calculator-react', // Replace with your GitHub repository name
-    assetPrefix: '/age-calculator-react/', // Replace with your GitHub repository name
+    distDir: 'build'
 }
 
 module.exports = nextConfig
